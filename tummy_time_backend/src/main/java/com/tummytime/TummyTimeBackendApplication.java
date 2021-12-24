cac74771220078f9b90a84c0cfc906ae17042f97
Geovani Ferreira <gugaheadbanger@gmail.com>
2021-12-24 09:02:01 -0300

    Inicio projeto de delivery

---
+++ tummy_time_backend/src/main/java/com/tummytime/TummyTimeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.tummytime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TummyTimeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TummyTimeBackendApplication.class, args);
+	}
+
+}
---
+++ tummy_time_backend/src/test/java/com/tummytime/TummyTimeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tummytime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TummyTimeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
